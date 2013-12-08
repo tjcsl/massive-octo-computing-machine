04333e995914727b7d738ea93510b65462ed0d68
Matthew Kaufer <mjkaufer@gmail.com>
2013-12-08 11:30:58 -0500

    Functionality.
    Clicking actually implements things. Try it out on the control center
    page. The personal money is also increased. Five of your clicks makes a
    dollar. Total group money is unchanged as I figured we could do that
    live from the backend. On the upgrade screen, upgrades are only shown as
    available if you can purchase them. This is dynamic, so if you have
    enough clicks for an upgrade, it'll tell you. Fun fun.

--- important.js
+++ important.js
@@ -1,11 +1,15 @@
 //Matthew is too cool.
 var numDivs = 8;
+var numPurchases = 4;
+var yourClicks = 400;
+
 $(function(){
     hideStuff(1);
+    updatePurchasable();
+    updateClicks();
 });
 function hideStuff(n){
     for(var i = 1; i <= numDivs; i++){
-        var s = '#div' + i;
         if(n!=i){
             $('#div' + i).hide();
             $('#men' + i).removeClass();
@@ -15,4 +19,30 @@ function hideStuff(n){
             $('#men' + i).removeClass().addClass('pure-menu-selected menu-item-divided');
         }
     }
+}
+
+function updatePurchasable(){//Are we going to base purchasing upgrades on click-count or on $$$ donated?
+	var x = parseInt($('#pclicks').text());
+	console.log(x);
+	for(var i = 1; i <= numPurchases; i++){
+		//based purely on click count
+		var cost = parseInt($('#cost' + i).text().split(",").join(""));
+		console.log(i + ":" + cost);
+		if(x >= cost)
+			$('#but' + i).removeClass().addClass("pure-button");
+		else
+			$('#but' + i).removeClass().addClass("pure-button pure-button-disabled");
+	}
+}
+function updateClicks(){
+	$('.pclicks').html(yourClicks);
+	//Below is code to update the donation based on the clicks. There'll likely be an algorithm to this, like 5 clicks is one dollar. Let's presume that that is the case.
+	var cash = yourClicks / 5.0; //algorithm
+	cash = (Math.round(cash*100)/100).toFixed(2); //rounding so the cash stays nice and formatted
+	$('.pdon').html(cash);
+}
+function increaseClicks(){
+	yourClicks++;
+	updateClicks();
+	updatePurchasable();	
 }
